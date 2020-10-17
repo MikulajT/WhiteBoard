349c36d8bb459672dedadd7053f6616aba7819c1
Tomas Mikulaj <mik0378@vsb.cz>
2020-10-17 22:29:20 +0200

    Přidání projektu

---
+++ WhiteBoard/Hubs/DrawDotHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhiteBoard.Hubs
+{
+    public class DrawDotHub : Hub
+    {
+        public async Task ClearCanvas()
+        {
+            await Clients.All.SendAsync("clearCanvas");
+        }
+    }
+}
---
+++ WhiteBoard/wwwroot/js/draw.js
@@ -0,0 +1,22 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/drawDotHub").build();
+
+connection.on("clearCanvas", function () {
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.clear();
+});
+
+connection.start().then(function () {
+    // nothing here
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+function tellServerToClear() {
+    connection.invoke("ClearCanvas").catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+let canvas = new fabric.Canvas('canvas', {
+    isDrawingMode: true
+});
